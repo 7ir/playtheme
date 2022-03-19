d5ec8b0017b1913bb1857f3c32eb906fdec78923
user.name <user.email>
2022-03-19 17:29:03 +0200

    Added slider functions but not frontend yet.

--- functions.php
+++ functions.php
@@ -21,3 +21,112 @@ function my_custom_theme_enqueue() {
 add_action( 'wp_enqueue_scripts', 'my_custom_theme_enqueue' );
 
 add_theme_support( 'title-tag' );
+
+
+/**
+ * ##########################################################
+ *   Slider
+ * ##########################################################
+ */
+
+/**
+ * Create Custom Post Type for Slideres
+ */
+function create_slider_post_type() {
+
+	$labels = array(
+		'name' => __( 'Sliders' ),
+		'singular_name' => __( 'Sliders' ),
+		'all_items'           => __( 'All Sliders' ),
+		'view_item'           => __( 'View Slider' ),
+		'add_new_item'        => __( 'Add New Slider' ),
+		'add_new'             => __( 'Add New Slider' ),
+		'edit_item'           => __( 'Edit Slider' ),
+		'update_item'         => __( 'Update Slider' ),
+		'search_items'        => __( 'Search Slider' ),
+		'search_items' => __('Sliders')
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'description' => 'Add New Slider contents',
+		'menu_position' => 27,
+		'public' => true,
+		'has_archive' => true,
+		'map_meta_cap' => true,
+		'capability_type' => 'post',
+		'hierarchical' => true,
+		'rewrite' => array('slug' => false),
+		'menu_icon'=>'dashicons-format-image',
+		'supports' => array(
+			'title',
+			'thumbnail','excerpt'
+		),
+	);
+	register_post_type( 'slider', $args);
+
+}
+add_action( 'init', 'create_slider_post_type' );
+
+/**
+ * Remove Slug support from custom post type
+ */
+add_action( 'init', function() {
+    remove_post_type_support( 'slider', 'editor' );
+    remove_post_type_support( 'slider', 'slug' );
+} );
+
+/**
+ * Add featured Image Support
+ */
+ function cih_theme_support(){
+
+   add_theme_support( 'post-thumbnails' );
+   add_image_size( 'slider_image','1024','720',true);
+
+}
+add_action('after_setup_theme','cih_theme_support');
+
+/**
+ * Add Link Field Creating Custom Meta-Box Field in WordPress Admin
+ */
+function sliderLink_add_meta_box() {
+   add_meta_box('slider_link','Slider Link','slider_link_callback','slider');
+}
+
+function slider_link_callback( $post ) {
+
+   wp_nonce_field('slider_link_save','slider_link_meta_box_nonce');
+   $value = get_post_meta($post->ID,'_slider_link_value_key',true);
+   ?>
+    <input type="text" name="slider_link_field" id="slider_link_field" value="<?php echo esc_attr( $value ); ?>" required="required" size="100" />
+   <?php
+}
+add_action('add_meta_boxes','sliderLink_add_meta_box');
+
+
+function slider_link_save( $post_id ) {
+   if( ! isset($_POST['slider_link_meta_box_nonce'])) {
+      return;
+   }
+   if( ! wp_verify_nonce( $_POST['slider_link_meta_box_nonce'], 'slider_link_save') ) {
+      return;
+   }
+   if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+      return;
+   }
+   if( ! current_user_can('edit_post', $post_id)) {
+      return;
+   }
+   if( ! isset($_POST['slider_link_field'])) {
+      return;
+   }
+   $slider_link = sanitize_text_field($_POST['slider_link_field']);
+   update_post_meta( $post_id,'_slider_link_value_key', $slider_link );
+}
+add_action('save_post','slider_link_save');
+
+
+/**
+ * ##########################################################
+ */
