b32a623f6ccca26db12ec86cdf3e0c133c17ec44
user.name <user.email>
2022-03-20 13:42:57 +0200

    Created second slider 2s after the first one, still same problem with timing. Will trigger next slide with custom clock.

--- footer.php
+++ footer.php
@@ -16,25 +16,28 @@
           },
         },
     })
-    var owl02 = jQuery('#owl-carousel-02');
-    //owl02.owlCarousel();
 
-    jQuery('#owl-carousel-02').owlCarousel({
-        autoplay:false,
-        autoplayTimeout: 2000,
-        smartSpeed: 500,
-        loop:true,
-        margin:10,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        nav:true,
-        responsive:{
-          0:{
-              items:1
+
+    setTimeout(function(){
+
+      jQuery('#owl-carousel-02').owlCarousel({
+          autoplay:true,
+          autoplayTimeout: 2000,
+          smartSpeed: 500,
+          loop:true,
+          margin:10,
+          animateIn: 'fadeIn',
+          animateOut: 'fadeOut',
+          nav:true,
+          responsive:{
+            0:{
+                items:1
+            },
           },
-        },
-    })
-    setTimeout(function(){ owl02.trigger('play.owl.autoplay') },2000);
+      })
+
+    },2000);
+
 
     </script>
 
