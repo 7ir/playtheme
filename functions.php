5c45af7574a55c59bb6b51181e78f9d8ce77717f
user.name <user.email>
2022-03-22 19:18:49 +0200

    I think I got it, now I need to put the full html into the string to echo

--- functions.php
+++ functions.php
@@ -191,150 +191,159 @@ add_action('save_post','qoute_text_save');
 //Adding shortcode action hook
 add_shortcode( 'hakuna-matata' , 'hakuna_matata_shortcode_callback');
 //Shortcode Callback function
-function hakuna_matata_shortcode_callback(){
+if( ! function_exists('hakuna_matata_shortcode_callback') ){
+  function hakuna_matata_shortcode_callback(){
 
 
-  /* Get cpt posts */
-  $qoutesQuery = new WP_Query( array( 'post_type' => 'slider' ) );
-  $qoutesPosts = $qoutesQuery->posts;
+    /* Get cpt posts */
+    $qoutesQuery = new WP_Query( array( 'post_type' => 'slider' ) );
+    $qoutesPosts = $qoutesQuery->posts;
 
-  /* Custom function to split arrays into equal parts, by Andrei from https://zerowp.com/ */
-  function splitMyArray(array $input_array, int $size, $preserve_keys = null): array
-  {
-      $nr = (int)ceil(count($input_array) / $size);
-      if ($nr > 0) {
-          return array_chunk($input_array, $nr, $preserve_keys);
-      }
-      return $input_array;
+    /* Custom function to split arrays into equal parts, by Andrei from https://zerowp.com/ */
+    function splitMyArray(array $input_array, int $size, $preserve_keys = null): array
+    {
+        $nr = (int)ceil(count($input_array) / $size);
+        if ($nr > 0) {
+            return array_chunk($input_array, $nr, $preserve_keys);
+        }
+        return $input_array;
+    }
+
+    /* hub for splitting arrays */
+    $splittedArray = splitMyArray($qoutesPosts, 4);
+    $splittedArray0 = $splittedArray[0];
+    $splittedArray1 = $splittedArray[1];
+    $splittedArray2 = $splittedArray[2];
+    $splittedArray3 = $splittedArray[3];
+    /*do_action('qm/info', array(
+      '$splittedArray0'=>$splittedArray0,
+      '$splittedArray1'=>$splittedArray1,
+      '$splittedArray2'=>$splittedArray2,
+      '$splittedArray3'=>$splittedArray3,
+    ));*/
+
+
+    //$post = get_option('post');
+    global $post;
+
+
+    $firstSlider = '';
+    foreach($splittedArray0 as $i => $post) {
+        setup_postdata($post);
+        $firstSlider .= '<div class="item">';
+        /*$firstSlider_02 = '<?php
+          if(has_post_thumbnail()):
+              ?><div class="featured_image_wrap"><?php
+                  the_post_thumbnail();
+              ?></div><?php
+          endif;
+          ?>
+          <h4 class=”post_title <?php the_title(); ?>”>
+            <?php the_title(); ?>
+          </h4>
+        </div>';*/
+        $firstSlider .= '</div>';
+    };
+
+
+    do_action('qm/info', $firstSlider );
+
+   return $firstSlider;
+    /*return '
+  <!-- Slider start -->
+  <section id="zlider" class="grid">
+
+
+    <div>1</div>
+
+    <div>
+    <div id="owl-carousel-01" class="owl-carousel owl-theme"><?php
+        $firstSlider
+        wp_reset_postdata(); ?>
+    </div>
+    </div>
+
+    <div>3</div>
+
+    <div>
+    <div id="owl-carousel-02" class="owl-carousel owl-theme"><?php
+        foreach($splittedArray1 as $i => $post) {
+            setup_postdata($post);
+            ?>
+            <div class="item"><?php
+              // use the template tags below here
+              if(has_post_thumbnail()):
+                  ?><div class="featured_image_wrap"><?php
+                      the_post_thumbnail();
+                  ?></div><?php
+              endif;
+              ?>
+              <h4 class=”post_title <?php the_title(); ?>”>
+                <?php the_title(); ?>
+              </h4>
+            </div>
+            <?php
+        };
+        wp_reset_postdata(); ?>
+    </div>
+    </div>
+
+    <div>5</div>
+
+    <div>
+    <div id="owl-carousel-03" class="owl-carousel owl-theme"><?php
+        foreach($splittedArray2 as $i => $post) {
+            setup_postdata($post);
+            ?>
+            <div class="item"><?php
+              // use the template tags below here
+              if(has_post_thumbnail()):
+                  ?><div class="featured_image_wrap"><?php
+                      the_post_thumbnail();
+                  ?></div><?php
+              endif;
+              ?>
+              <h4 class=”post_title <?php the_title(); ?>”>
+                <?php the_title(); ?>
+              </h4>
+            </div>
+            <?php
+        };
+        wp_reset_postdata(); ?>
+    </div>
+    </div>
+
+    <div>7</div>
+
+    <div>
+    <div id="owl-carousel-04" class="owl-carousel owl-theme"><?php
+        foreach($splittedArray3 as $i => $post) {
+            setup_postdata($post);
+            ?>
+            <div class="item"><?php
+              // use the template tags below here
+              if(has_post_thumbnail()):
+                  ?><div class="featured_image_wrap"><?php
+                      the_post_thumbnail();
+                  ?></div><?php
+              endif;
+              ?>
+              <h4 class=”post_title <?php the_title(); ?>”>
+                <?php the_title(); ?>
+              </h4>
+            </div>
+            <?php
+        };
+        wp_reset_postdata(); ?>
+    </div>
+    </div>
+
+    <div>9</div>
+
+  </section> <!-- Slider end -->
+
+  ';*/
   }
-
-  /* hub for splitting arrays */
-  $splittedArray = splitMyArray($qoutesPosts, 4);
-  $splittedArray0 = $splittedArray[0];
-  $splittedArray1 = $splittedArray[1];
-  $splittedArray2 = $splittedArray[2];
-  $splittedArray3 = $splittedArray[3];
-  do_action('qm/info', array(
-    '$splittedArray0'=>$splittedArray0,
-    '$splittedArray1'=>$splittedArray1,
-    '$splittedArray2'=>$splittedArray2,
-    '$splittedArray3'=>$splittedArray3,
-  ));
-
-
-  global $post;
-
-
-  return '
-<!-- Slider start -->
-<section id="zlider" class="grid">
-
-
-  <div>1</div>
-
-  <div>
-  <div id="owl-carousel-01" class="owl-carousel owl-theme"><?php
-      foreach($splittedArray0 as $i => $post) {
-          setup_postdata($post);
-          ?>
-          <div class="item"><?php
-            // use the template tags below here
-            if(has_post_thumbnail()):
-                ?><div class="featured_image_wrap"><?php
-                    the_post_thumbnail();
-                ?></div><?php
-            endif;
-            ?>
-            <h4 class=”post_title <?php the_title(); ?>”>
-              <?php the_title(); ?>
-            </h4>
-          </div>
-          <?php
-      };
-      wp_reset_postdata(); ?>
-  </div>
-  </div>
-
-  <div>3</div>
-
-  <div>
-  <div id="owl-carousel-02" class="owl-carousel owl-theme"><?php
-      foreach($splittedArray1 as $i => $post) {
-          setup_postdata($post);
-          ?>
-          <div class="item"><?php
-            // use the template tags below here
-            if(has_post_thumbnail()):
-                ?><div class="featured_image_wrap"><?php
-                    the_post_thumbnail();
-                ?></div><?php
-            endif;
-            ?>
-            <h4 class=”post_title <?php the_title(); ?>”>
-              <?php the_title(); ?>
-            </h4>
-          </div>
-          <?php
-      };
-      wp_reset_postdata(); ?>
-  </div>
-  </div>
-
-  <div>5</div>
-
-  <div>
-  <div id="owl-carousel-03" class="owl-carousel owl-theme"><?php
-      foreach($splittedArray2 as $i => $post) {
-          setup_postdata($post);
-          ?>
-          <div class="item"><?php
-            // use the template tags below here
-            if(has_post_thumbnail()):
-                ?><div class="featured_image_wrap"><?php
-                    the_post_thumbnail();
-                ?></div><?php
-            endif;
-            ?>
-            <h4 class=”post_title <?php the_title(); ?>”>
-              <?php the_title(); ?>
-            </h4>
-          </div>
-          <?php
-      };
-      wp_reset_postdata(); ?>
-  </div>
-  </div>
-
-  <div>7</div>
-
-  <div>
-  <div id="owl-carousel-04" class="owl-carousel owl-theme"><?php
-      foreach($splittedArray3 as $i => $post) {
-          setup_postdata($post);
-          ?>
-          <div class="item"><?php
-            // use the template tags below here
-            if(has_post_thumbnail()):
-                ?><div class="featured_image_wrap"><?php
-                    the_post_thumbnail();
-                ?></div><?php
-            endif;
-            ?>
-            <h4 class=”post_title <?php the_title(); ?>”>
-              <?php the_title(); ?>
-            </h4>
-          </div>
-          <?php
-      };
-      wp_reset_postdata(); ?>
-  </div>
-  </div>
-
-  <div>9</div>
-
-</section> <!-- Slider end -->
-
-';
 }
 
 
